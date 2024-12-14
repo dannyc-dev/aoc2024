8c7e1fbfcbcf2f03ec47b40ffc1c6e78c4ef85ab
dannyc-dev <dannyc10@vt.edu>
2024-12-14 09:28:57 -0500

    day13 part 2

--- day13/sol.py
+++ day13/sol.py
@@ -55,4 +55,38 @@ def part1():
 
 
 def part2():
-    pass
+   
+    def calc_prize(ax, ay, bx, by, px, py):
+        a_presses = ((px * by) - (py * bx)) / ((ax * by) - (ay * bx))
+        b_presses = (px - ax * a_presses) / bx
+
+        if a_presses % 1 == 0 and b_presses % 1 == 0:
+            return int(a_presses * 3 + b_presses)
+
+        return 0
+    
+    ans = 0
+    for machine in machines:
+        vars = []
+        for i in range(3):
+            if i < 2:
+                nums = re.findall(r"\+(\d+)", machine[i])
+                vars += nums
+            else:
+                nums = re.findall(r"\=(\d+)", machine[i])
+                vars += nums
+
+        min_tokens = calc_prize(
+            int(vars[0]),
+            int(vars[1]),
+            int(vars[2]),
+            int(vars[3]),
+            int(vars[4]) + 10000000000000,
+            int(vars[5]) + 10000000000000,
+        )
+
+        ans += min_tokens
+
+    return ans
+
+print(part2())
